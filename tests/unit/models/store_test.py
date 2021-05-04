b6579f2ae3e9e5ebdf78b038044ab6f7d95b0bfc
allyboy08 <alexsassman65@gmail.com>
2021-05-04 08:38:23 +0200

    update

--- tests/unit/models/store_test.py
+++ tests/unit/models/store_test.py
@@ -10,32 +10,6 @@ class StoreTest(BaseTest):
         self.assertListEqual(store.items.all(), [],
                              "The store's items length was not 0 even though no items were added.")
         
-    def test_crud(self):
-        with self.app_context():
-            store = StoreModel('test')
-
-            self.assertIsNone(StoreModel.find_by_name('test'), "Found an store with name 'test' before save_to_db")
-
-            store.save_to_db()
-
-            self.assertIsNotNone(StoreModel.find_by_name('test'),
-                                 "Did not find an store with name 'test' after save_to_db")
-
-            store.delete_from_db()
-
-            self.assertIsNone(StoreModel.find_by_name('test'), "Found an store with name 'test' after delete_from_db")
-
-    def test_store_relationship(self):
-        with self.app_context():
-            store = StoreModel('test')
-            item = ItemModel('test_item', 19.99, 1)
-
-            store.save_to_db()
-            item.save_to_db()
-
-            self.assertEqual(store.items.count(), 1)
-            self.assertEqual(store.items.first().name, 'test_item')
-
     def test_store_json(self):
         store = StoreModel('test')
         expected = {
@@ -46,16 +20,4 @@ class StoreTest(BaseTest):
 
         self.assertDictEqual(store.json(), expected)
         
-    def test_store_json_with_item(self):
-            with self.app_context():
-                store = StoreModel('test')
-                item = ItemModel('test_item', 19.99, 1)
-                
-                store.save_to_db()
-                item.save_to_db()
-                expected = {
-                    'name': 'test',
-                    'items': [{'name': 'test_item', 'price': 19.99}]
-                }
-                
-                self.assertDictEqual(store.json(), expected)  
+    
